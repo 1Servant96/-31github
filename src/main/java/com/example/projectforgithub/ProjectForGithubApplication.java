5fcc129a7955542da293116daacd535a9ddfa287
Nursultan Zhumataev <zhumataev.03@gmail.com>
2023-11-06 16:05:37 -0600

    first commit

---
+++ src/main/java/com/example/projectforgithub/ProjectForGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectforgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectForGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectforgithub/ProjectForGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectforgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
