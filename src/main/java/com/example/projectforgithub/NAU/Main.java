3738250ad68643e2bd8a491a20d8fe002da8aa43
Nursultan Zhumataev <zhumataev.03@gmail.com>
2023-11-07 22:10:20 -0600

    second commit

--- src/main/java/com/example/projectforgithub/NAU/Main.java
+++ src/main/java/com/example/projectforgithub/NAU/Main.java
@@ -3,5 +3,6 @@ package com.example.projectforgithub.NAU;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Inshalla here will be what can I show people, I hope");
     }
 }
