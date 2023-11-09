1d38a145627bd8285c8c93707e126a8f76dd5388
Nursultan Zhumataev <zhumataev.03@gmail.com>
2023-11-08 23:55:08 -0600

    third commit

--- src/main/java/com/example/projectforgithub/NAU/Main.java
+++ src/main/java/com/example/projectforgithub/NAU/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Inshalla here will be what can I show people, I hope");
+        System.out.println("Third commit for now");
     }
 }
