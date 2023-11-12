211432df450acc12b537355646c32c4da12afda6
Nursultan Zhumataev <zhumataev.03@gmail.com>
2023-11-12 16:48:55 -0600

    3rd commit 2.0

--- src/main/java/com/example/projectforgithub/NAU/Main.java
+++ src/main/java/com/example/projectforgithub/NAU/Main.java
@@ -9,5 +9,6 @@ public class Main {
                 "Wondering to help people.");
         System.out.println("Reincarnation, hope exists");
         System.out.println("Today is the happy birthday of @dtaalai, creator of this challenge, to whom why this repository exists.");
+        System.out.println("Fixed the commit from 3nd to 3rd.");
     }
 }
